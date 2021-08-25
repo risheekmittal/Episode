3b137f266fd23d28912d51baaa1e8111f92e97d5
risheekmittal <47297497+risheekmittal@users.noreply.github.com>
2021-08-25 23:03:35 +0530

    Add files via upload

---
+++ lib/story.dart
@@ -0,0 +1,13 @@
+class Story{
+
+    String r;
+    String s;
+    String t;
+
+    Story({String storyTitle, String choice1, String choice2}){
+        r = storyTitle;
+        s = choice1;
+        t = choice2;
+    }
+
+}
